6d127eafc5481123a3bbd51696e828830f2c0ea0
Zachary Zaro <zach@percolate.com>
2013-02-28 14:52:33 -0500

    add template to the app

--- main.py
+++ main.py
@@ -15,10 +15,17 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
